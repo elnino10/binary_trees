e3123a84df8457bcb3b2b878b9641a5541e6d325
Joe Egboka <joeegboka@gmail.com>
2024-01-05 22:23:35 +0100

    modify 16-binary_tree_is_perfect.c file

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -16,10 +16,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	left_node = binary_tree_height(tree->left);
 	right_node = binary_tree_height(tree->right);
 
-	if (left_node == right_node)
-		return (1);
-
-	return (0);
+	return (left_node == right_node);
 }
 
 /**
@@ -35,10 +32,13 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+	if (tree->left || tree->right)
+	{
+		left_height = binary_tree_height(tree->left);
+		right_height = binary_tree_height(tree->right);
 
-	return ((left_height > right_height ? left_height : right_height) + 1);
+		return ((left_height > right_height ? left_height : right_height) + 1);
+	}
 
 	return (0);
 }
